f97753ed599a400d46515340e24387a77ab912e0
Youngjune Kwon <shining@uvas.kr>
2021-07-07 12:37:23 +0900

    root ca 인증서 생성코드 추가

---
+++ 0_generate_root_ca.sh
@@ -0,0 +1,51 @@
+#/bin/bash
+
+if [ ! $1 ]; then
+    echo usage : $0 org_name
+    exit
+fi
+
+NAME=$1
+
+mkdir -p tls/certs tls/keys tls/conf
+
+openssl genrsa -aes256 -out tls/keys/${NAME}-rootca.key 2048
+
+cat <<EOT >> tls/conf/rootca_openssl.conf
+[ req ]
+default_bits            = 2048
+default_md              = sha1
+default_keyfile         = ${NAME}-rootca.key
+distinguished_name      = req_distinguished_name
+extensions              = v3_ca
+req_extensions          = v3_ca
+
+[ v3_ca ]
+basicConstraints       = critical, CA:TRUE, pathlen:0
+subjectKeyIdentifier   = hash
+##authorityKeyIdentifier = keyid:always, issuer:always
+keyUsage               = keyCertSign, cRLSign
+nsCertType             = sslCA, emailCA, objCA
+
+[ req_distinguished_name ]
+countryName                     = Country Name (2 letter code)
+countryName_default             = KR
+countryName_min                 = 2
+countryName_max                 = 2
+
+organizationName              = Organization Name (eg, company)
+organizationName_default      = ${NAME} Inc.
+
+commonName                      = Common Name (eg, your name or your server's hostname)
+commonName_default             = ${NAME}'s Self Signed CA
+commonName_max                  = 64
+EOT
+
+openssl req -new -key tls/keys/${NAME}-rootca.key -out tls/certs/${NAME}-rootca.csr -config tls/conf/rootca_openssl.conf
+openssl x509 -req -days 3650 -extensions v3_ca -set_serial 1 \
+    -in tls/certs/${NAME}-rootca.csr \
+    -signkey tls/keys/${NAME}-rootca.key \
+    -out tls/certs/${NAME}-rootca.crt \
+    -extfile tls/conf/rootca_openssl.conf
+
+openssl x509 -text -in tls/certs/${NAME}-rootca.crt
---
+++ README.md
@@ -0,0 +1,7 @@
+# Usage
+
+./0_generate_root_ca.sh org_name
+
+# Reference
+
+https://www.lesstif.com/system-admin/openssl-root-ca-ssl-6979614.html
